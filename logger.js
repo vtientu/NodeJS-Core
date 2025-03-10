19859215b163a037d22a6b95011c02aafb7f4156
TuVT <tientu115@gmail.com>
2025-03-10 22:43:42 +0700

    2025/03/10T22:43 - First commit NodeJS Core

---
+++ logger.js
@@ -0,0 +1,33 @@
+import winston from "winston";
+import DailyRotateFile from "winston-daily-rotate-file";
+
+const logFormat = winston.format.combine(
+    winston.format.timestamp({
+        format: "HH:mm:ss DD-MM-YYYY"
+    }),
+    winston.format.printf(({ timestamp, level, message }) => {
+        return `${timestamp} [${level.toUpperCase()}]: ${message}`
+    })
+)
+
+const logger = winston.createLogger({
+    level: "info",
+    format: logFormat,
+    transports: [
+        new winston.transports.Console(),
+        new DailyRotateFile({
+            filename: "./log/app-%DATE%.log",
+            datePattern: "DD-MM-YYYY",
+            maxFiles: "14d",
+            zippedArchive: true
+        })
+    ]
+})
+
+logger.add(
+    new winston.transports.Console({
+        format: winston.format.simple()
+    })
+)
+
+export default logger;
