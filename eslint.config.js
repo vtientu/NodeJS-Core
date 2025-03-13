f60b21ebb10cfc06c5c8d8d5ba3a06302e22d9ce
TuDev <tientu115@gmail.com>
2025-03-13 17:03:51 +0700

    Update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,35 +1,35 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import eslintPluginPrettier from "eslint-plugin-prettier";
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
     plugins: {
-      prettier: eslintPluginPrettier,
+      prettier: eslintPluginPrettier
     },
     rules: {
-      "@typescript-eslint/no-explicit-any": "warn",
-      "@typescript-eslint/no-unused-vars": "warn",
-      "prettier/prettier": [
-        "warn",
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': false,
+      'prettier/prettier': [
+        'warn',
         {
-          arrowParens: "always",
+          arrowParens: 'always',
           semi: false,
-          trailingComma: "none",
+          trailingComma: 'none',
           tabWidth: 2,
-          endOfLine: "auto",
+          endOfLine: 'auto',
           useTabs: false,
           singleQuote: true,
           printWidth: 120,
-          jsxSingleQuote: true,
-        },
-      ],
+          jsxSingleQuote: true
+        }
+      ]
     },
-    ignores: ["**/node_modules/", "**/dist/"],
-  },
-];
+    ignores: ['**/node_modules/', '**/dist/', 'eslint.config.js']
+  }
+]
--- server.ts
+++ server.ts
@@ -1,13 +1,10 @@
 import logger from '@/v1/utils/logger.js'
-import 'dotenv/config'
 import database from '@/v1/database/database.js'
 import app from '@/app.js'
+import 'dotenv/config'
 
 const PORT = process.env.PORT_SERVER || 3000
 
-logger.error(`🔥 Lỗi server: `)
-logger.info(`🔥 Logger server: `)
-//Connect DB
 database.connectDB()
 
 const server = app.listen(PORT, () => {
@@ -15,7 +12,7 @@ const server = app.listen(PORT, () => {
 })
 
 server.on('error', (err: any) => {
-  if ((err as any).code === 'EADDRINUSE') {
+  if (err.code === 'EADDRINUSE') {
     logger.error(`Cổng ${PORT} đã bị chiếm!`)
   } else {
     logger.error(`🔥 Lỗi server: ${JSON.stringify(err, null, 2)}`)
