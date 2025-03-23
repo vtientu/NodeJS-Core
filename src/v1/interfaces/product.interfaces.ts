59edbe6f6519176ca4f3ea455f880f7545c6c3a4
TuVT <tientu115@gmail.com>
2025-03-23 19:03:17 +0700

    Product schema ecommerce

---
+++ src/v1/interfaces/product.interfaces.ts
@@ -0,0 +1,27 @@
+import { Document, Schema } from 'mongoose'
+
+export interface IProduct extends Document {
+  _id: Schema.Types.ObjectId
+  product_name: string
+  product_description: string
+  product_thumb: string
+  product_price: number
+  product_quantity: number
+  product_type: string
+  product_shop: Schema.Types.ObjectId
+  product_attribute: Schema.Types.Mixed
+}
+
+export interface IClothing extends Document {
+  _id: Schema.Types.ObjectId
+  brand: string
+  size: string
+  material: string
+}
+
+export interface IElectronic extends Document {
+  _id: Schema.Types.ObjectId
+  manufacturer: string
+  form: string
+  color: string
+}
---
+++ src/v1/models/product.model.ts
@@ -0,0 +1,81 @@
+import { IClothing, IElectronic, IProduct } from '@interfaces/product.interfaces.js'
+import { Schema, model } from 'mongoose'
+
+const COLLECTION_NAME = 'Products'
+const DOCUMENT_NAME = 'Product'
+
+const ProductSchema = new Schema(
+  {
+    product_name: {
+      type: String,
+      required: true,
+      index: true
+    },
+    product_thumb: {
+      type: String,
+      required: true
+    },
+    product_description: String,
+    product_price: {
+      type: Number,
+      required: true
+    },
+    product_quantity: {
+      type: Number,
+      required: true
+    },
+    product_type: {
+      type: String,
+      required: true,
+      enum: ['Electronic', 'Clothing', 'Furniture']
+    },
+    product_shop: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    product_attribute: {
+      type: Schema.Types.Mixed,
+      required: true
+    }
+  },
+  {
+    timestamps: true,
+    collection: COLLECTION_NAME
+  }
+)
+
+const ClothingSchema = new Schema(
+  {
+    brand: {
+      type: String,
+      required: true
+    },
+    size: String,
+    material: String
+  },
+  {
+    timestamps: true,
+    collection: 'Clothes'
+  }
+)
+
+const ElectronicSchema = new Schema(
+  {
+    manufacturer: {
+      type: String,
+      required: true
+    },
+    form: String,
+    color: String
+  },
+  {
+    timestamps: true,
+    collection: 'Electronics'
+  }
+)
+
+const ProductModel = model<IProduct>(DOCUMENT_NAME, ProductSchema)
+export const ClothingModel = model<IClothing>('Clothing', ClothingSchema)
+export const ElectronicModel = model<IElectronic>('Electronic', ElectronicSchema)
+export default ProductModel
